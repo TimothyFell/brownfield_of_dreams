9c0f8633ae0694eb9e26b24d8daf011c9d6c5fdd
TimothyFell <timothy.fell846@gmail.com>
2018-12-05 14:55:18 -0700

    add sad path tests for github repos on user dashboard

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 feature 'A user visits their dashboard' do
   scenario 'can see all github repos' do
+    user_1 = create(:user, token: 'token mole')
+    user_2 = create(:user, token: 'token pizza')
 
     stub_request(:get, "https://api.github.com/user/repos").
-                  to_return(body: File.read("./spec/fixtures/github_repos_fixture.json"))
+      with(headers: {"Authorization" => 'token mole'}).
+      to_return(body: File.read("./spec/fixtures/github_repos_fixture.json"))
 
-    user = create(:user, token: ENV['GITHUB_API_KEY_1'])
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     visit '/dashboard'
 
@@ -16,16 +18,27 @@ feature 'A user visits their dashboard' do
   end
 
   scenario 'cant see another users repos' do
+    user_1 = create(:user, token: 'token mole')
+    user_2 = create(:user, token: 'token pizza')
+
     stub_request(:get, "https://api.github.com/user/repos").
+      with(headers: {"Authorization" => 'token mole'}).
       to_return(body: File.read("./spec/fixtures/github_repos_fixture.json"))
 
-    user_1 = create(:user, token: ENV['GITHUB_API_KEY_1'])
-    user_2s_data = JSON.parse(File.read("./spec/fixtures/github_repos_2_fixture.json")).first
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_2)
+
+    expect { visit '/dashboard'}.to raise_error(ActionView::Template::Error)
+  end
+
+  scenario 'user without token does not see repos' do
+    user_1 = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     visit '/dashboard'
 
-    expect(page).to_not have_css(".repo_id_#{user_2s_data['id']}")
+    expect(page).to_not have_content("GitHub Repositories")
+    expect(page).to_not have_css(".github_repo")
   end
 end
